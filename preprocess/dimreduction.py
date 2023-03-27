29a6c618804adbe93b23883cd74bb83e58097b60
Elia Marcantognini <elia.marcantognini@gmail.com>
2023-03-27 13:17:15 +0200

    Create dimreduction.py

---
+++ preprocess/dimreduction.py
@@ -0,0 +1,6 @@
+from sklearn.decomposition import PCA
+import numpy as np
+# features_ndarray = features...
+# sk_model = PCA(n_components=10)
+# sk_model.fit_transform(features_ndarray)
+# print(sk_model.explained_variance_ratio_.cumsum())
