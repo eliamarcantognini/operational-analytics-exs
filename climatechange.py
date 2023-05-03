41b49a6df5d110fdc030b13e905e0725fb728d82
Elia Marcantognini <elia.marcantognini@gmail.com>
2023-05-03 17:49:08 +0200

    decomment lines

--- climatechange.py
+++ climatechange.py
@@ -17,12 +17,12 @@ df.set_index('dt', inplace=True)
 print('after cleaning')
 print(df.info())
 
-# df.plot(x='dt', y='AverageTemperature', title='Rome temperature')
-# plt.show()
-#
-# for i in range (1, 13):
-#     df[df.dt.dt.month == i].plot(x='dt', y='AverageTemperature', title='Rome temperature in month ' + str(i))
-#     plt.show()
+df.plot(x='dt', y='AverageTemperature', title='Rome temperature')
+plt.show()
+
+for i in range (1, 13):
+    df[df.dt.dt.month == i].plot(x='dt', y='AverageTemperature', title='Rome temperature in month ' + str(i))
+    plt.show()
 
 decomposition = seasonal_decompose(df)
 plt.plot(decomposition.trend)
@@ -30,28 +30,28 @@ plt.show()
 plt.plot(decomposition.seasonal)
 plt.show()
 
-# cutpoint = int(len(df) * 0.7)
-# train = df[:cutpoint]
-# test = df[cutpoint:]
+cutpoint = int(len(df) * 0.7)
+train = df[:cutpoint]
+test = df[cutpoint:]
 
-# model = pm.auto_arima(train, start_p=1, start_q=1, test='adf', max_p=3, max_q=3, m=4, start_P=0, seasonal=True,
-#                       d=None, D=1, trace=True, error_action='ignore', suppress_warnings=True,
-#                       stepwise=True)  # stepwise=False full grid
-#
-# print(model.summary())
-# morder = model.order  # p,d,q
-# print("Sarimax order {0}".format(morder))
-# mseasorder = model.seasonal_order  # P,D,Q,m
-# print("Sarimax seasonal order {0}".format(mseasorder))
-#
-# fitted = model.fit(train)
-# yfore = fitted.predict(n_periods=12)  # forecast
-# ypred = fitted.predict_in_sample()
-# plt.plot(df, label='df')
-# plt.plot(ypred, label='ypred')
-# plt.plot([None for i in ypred] + [x for x in yfore], label='yfore')
-# plt.xlabel('time')
-# plt.ylabel('sales')
-# plt.legend()
-# plt.show()
+model = pm.auto_arima(train, start_p=1, start_q=1, test='adf', max_p=3, max_q=3, m=4, start_P=0, seasonal=True,
+                      d=None, D=1, trace=True, error_action='ignore', suppress_warnings=True,
+                      stepwise=True)  # stepwise=False full grid
+
+print(model.summary())
+morder = model.order  # p,d,q
+print("Sarimax order {0}".format(morder))
+mseasorder = model.seasonal_order  # P,D,Q,m
+print("Sarimax seasonal order {0}".format(mseasorder))
+
+fitted = model.fit(train)
+yfore = fitted.predict(n_periods=12)  # forecast
+ypred = fitted.predict_in_sample()
+plt.plot(df, label='df')
+plt.plot(ypred, label='ypred')
+plt.plot([None for i in ypred] + [x for x in yfore], label='yfore')
+plt.xlabel('time')
+plt.ylabel('sales')
+plt.legend()
+plt.show()
 
