28252990b2999e46d8a203a3405cf9d13802b21a
Elia Marcantognini <elia.marcantognini@gmail.com>
2023-06-20 11:44:33 +0200

    Update marcantognini_power_consumption.py

--- assignments/marcantognini_power_consumption.py
+++ assignments/marcantognini_power_consumption.py
@@ -47,22 +47,21 @@ print('----------------')
 #####################
 #####################
 # drop columns
-data.drop(columns=['Global_reactive_power', 'Global_intensity', 'Time', 'Voltage', 'Sub_metering_1', 'Sub_metering_2',
-                   'Sub_metering_3'],
-          inplace=True)
+df = data.drop(columns=['Global_reactive_power', 'Global_intensity', 'Time', 'Voltage', 'Sub_metering_1', 'Sub_metering_2',
+                   'Sub_metering_3'])
 # convert to datetime
-data['Date'] = pd.to_datetime(data['Date'], format='%d/%m/%Y')
+df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
 # convert to numeric
-data['Global_active_power'] = pd.to_numeric(data['Global_active_power'], errors='coerce', downcast='float')
-data.rename(columns={'Date': 'Month', 'Global_active_power': 'Active'}, inplace=True)
+df['Global_active_power'] = pd.to_numeric(df['Global_active_power'], errors='coerce', downcast='float')
+df.rename(columns={'Date': 'Month', 'Global_active_power': 'Active'}, inplace=True)
 
 # sum power consumption per month
-data = data.groupby('Month').sum()
+df = df.groupby('Month').sum()
 # resample to monthly
-data = data.groupby(pd.Grouper(freq='M')).sum()
+df = df.groupby(pd.Grouper(freq='M')).sum()
 # drop first month (incomplete)
 # drop last month (incomplete)
-df = data.drop([data.index[0], data.index[-1]])
+df = df.drop([df.index[0], df.index[-1]])
 # the last year is incomplete, use it as test set
 # there is some missing data and some zeros, fill with the mean and ffill
 df.Active[df.Active == 0.0] = df.Active.mean()
@@ -145,93 +144,70 @@ test_set = active[train_size:].values
 train_index = active[:train_size].index
 test_index = active[train_size:].index
 
-# #####################
-# #####################
-# # models
-# #####################
-# #####################
-#
-# plt.figure(figsize=(15, 15))
-#
-# # sarima model
-# sarima_model = pm.auto_arima(train_set, start_p=1, start_q=1, test='adf', max_p=5, max_q=5, m=4,
-#                              start_P=0, seasonal=True, d=None, D=0, trace=True,
-#                              error_action='ignore', suppress_warnings=True, stepwise=False,
-#                              n_jobs=-1)
-# sarima_model = sarima_model.fit(train_set)
-# sarima_forecast = sarima_model.predict(n_periods=len(test_set))
-# sarima_forecast_series = pd.Series([invert_boxcox(x, lmbda) for x in sarima_forecast], index=active[train_size:].index)
-# plt.subplot(4, 1, 1)
-# plt.plot(df.Active, label='data')
-# plt.plot(sarima_forecast_series, label='sarima')
-# plt.title('SARIMA - RMSE: %.2f' % rmse(test_set, sarima_forecast))
-# plt.legend()
-#
-# # lstm model
-# lstm_model = Sequential()
-# lstm_model.add(LSTM(50, activation='relu', input_shape=(1, 1)))
-# lstm_model.add(Dense(1))
-# lstm_model.compile(optimizer='adam', loss='mse')
-# lstm_model.summary()
-# # reshape input to be 3D [samples, timesteps, features]
-# train_active_reshaped = train_set.reshape((len(train_set), 1, 1))
-# test_active_reshaped = test_set.reshape((len(test_set), 1, 1))
-# # fit model
-# lstm_model.fit(train_active_reshaped, train_active_reshaped, epochs=200, verbose=0)
-# # make a prediction
-# lstm_forecast = lstm_model.predict(test_active_reshaped)
-# lstm_forecast_series = pd.Series([invert_boxcox(x, lmbda) for x in lstm_forecast], index=active[train_size:].index)
-# plt.subplot(4, 1, 2)
-# plt.plot(df.Active, label='data')
-# plt.plot(lstm_forecast_series, label='lstm')
-# plt.title('LSTM - RMSE: %.2f' % rmse(test_set, lstm_forecast.flatten()))
-# plt.legend()
-#
-# # mlp model
-# mlp_model = Sequential()
-# mlp_model.add(Dense(50, activation='relu', input_dim=1))
-# mlp_model.add(Dense(1))
-# mlp_model.compile(optimizer='adam', loss='mse')
-# mlp_model.summary()
-# # fit model
-# mlp_model.fit(train_active_reshaped, train_active_reshaped, epochs=200, verbose=0)
-# # make a prediction
-# mlp_forecast = mlp_model.predict(test_active_reshaped)
-# mlp_forecast_series = pd.Series([invert_boxcox(x, lmbda) for x in mlp_forecast], index=active[train_size:].index)
-# plt.subplot(4, 1, 3)
-# plt.plot(df.Active, label='data')
-# plt.plot(mlp_forecast_series, label='mlp')
-# plt.title('MLP - RMSE: %.2f' % rmse(test_set, mlp_forecast.flatten()))
-# plt.legend()
-#
-# # compare models
-# plt.subplot(4, 1, 4)
-# plt.plot(df.Active[train_size:], label='data')
-# plt.plot(sarima_forecast_series, label='sarima')
-# plt.plot(lstm_forecast_series, label='lstm')
-# plt.plot(mlp_forecast_series, label='mlp')
-# plt.title('Comparison of models')
-# plt.legend()
-# plt.show()
-#
-# print('------------------\nMODEL COMPARISON\n------------------')
-# print('SARIMA - RMSE: %.4f' % rmse(test_set, sarima_forecast))
-# print('LSTM - RMSE: %.4f' % rmse(test_set, lstm_forecast.flatten()))
-# print('MLP - RMSE: %.4f' % rmse(test_set, mlp_forecast.flatten()))
-# print(
-#     'MLPvsLSTM - DM: p-value: %.4f DM: %.4f ' % dm_test(test_set, lstm_forecast.flatten(), mlp_forecast.flatten(), h=1,
-#                                                         crit='MSE'))
-# # MLPvsLSTM - DM: p-value: 5.1069 DM: 0.0006
-# # with 5% significance level, the p-value is less than 0.025 and z-value is greater than 1.96,
-# # so we can reject the null hypothesis that the two models have the same performance
-# print(
-#     'MLP seems better than LSTM, but in the DM test the p-value is not significant,'
-#     ' so we reject the null hypothesis that the two models have the same performance.')
-#
-# # MLP and LSTM seem to be the best models, but they are trained with a small dataset
-# # neural networks are good for time series forecasting, but they need a lot of data
-# # so let's try ML tree models: random forest and XGBoost. They are also hungry for data
-# # so we expect them to perform worse than neural networks
+#####################
+#####################
+# models
+#####################
+#####################
+
+plt.figure(figsize=(15, 20))
+
+# sarima model
+sarima_model = pm.auto_arima(train_set, start_p=1, start_q=1, test='adf', max_p=5, max_q=5, m=4,
+                             start_P=0, seasonal=True, d=None, D=0, trace=True,
+                             error_action='ignore', suppress_warnings=True, stepwise=False,
+                             n_jobs=-1)
+sarima_model = sarima_model.fit(train_set)
+sarima_forecast = sarima_model.predict(n_periods=len(test_set))
+sarima_forecast_series = pd.Series([invert_boxcox(x, lmbda) for x in sarima_forecast], index=active[train_size:].index)
+plt.subplot(6, 1, 1)
+plt.plot(df.Active, label='data')
+plt.plot(sarima_forecast_series, label='sarima')
+plt.title('SARIMA - RMSE: %.2f' % rmse(test_set, sarima_forecast))
+plt.legend()
+
+# lstm model
+lstm_model = Sequential()
+lstm_model.add(LSTM(50, activation='relu', input_shape=(1, 1)))
+lstm_model.add(Dense(1))
+lstm_model.compile(optimizer='adam', loss='mse')
+lstm_model.summary()
+# reshape input to be 3D [samples, timesteps, features]
+train_active_reshaped = train_set.reshape((len(train_set), 1, 1))
+test_active_reshaped = test_set.reshape((len(test_set), 1, 1))
+# fit model
+lstm_model.fit(train_active_reshaped, train_active_reshaped, epochs=200, verbose=0)
+# make a prediction
+lstm_forecast = lstm_model.predict(test_active_reshaped)
+lstm_forecast_series = pd.Series([invert_boxcox(x, lmbda) for x in lstm_forecast], index=active[train_size:].index)
+plt.subplot(6, 1, 2)
+plt.plot(df.Active, label='data')
+plt.plot(lstm_forecast_series, label='lstm')
+plt.title('LSTM - RMSE: %.2f' % rmse(test_set, lstm_forecast.flatten()))
+plt.legend()
+
+# mlp model
+mlp_model = Sequential()
+mlp_model.add(Dense(50, activation='relu', input_dim=1))
+mlp_model.add(Dense(1))
+mlp_model.compile(optimizer='adam', loss='mse')
+mlp_model.summary()
+# fit model
+mlp_model.fit(train_active_reshaped, train_active_reshaped, epochs=200, verbose=0)
+# make a prediction
+mlp_forecast = mlp_model.predict(test_active_reshaped)
+mlp_forecast_series = pd.Series([invert_boxcox(x, lmbda) for x in mlp_forecast], index=active[train_size:].index)
+plt.subplot(6, 1, 3)
+plt.plot(df.Active, label='data')
+plt.plot(mlp_forecast_series, label='mlp')
+plt.title('MLP - RMSE: %.2f' % rmse(test_set, mlp_forecast.flatten()))
+plt.legend()
+
+
+# MLP and LSTM seem to be the best models, but they are trained with a small dataset
+# neural networks are good for time series forecasting, but they need a lot of data
+# let's try also ML tree models: random forest and XGBoost. They are also hungry for data
+# so we expect them to perform worse than neural networks
 
 train_set = train_set.reshape(-1, 1)
 test_set = test_set.reshape(-1, 1)
@@ -248,26 +224,53 @@ def invert_transformations(values):
     return res
 
 # random forest
-rf_model = RandomForestRegressor(max_depth=5, n_estimators=50)
+rf_model = RandomForestRegressor(max_depth=2, n_estimators=100)
 rf_model.fit(train_set, train_index)
 rf_forecast = rf_model.predict(test_set)
 rf_forecast_series = pd.Series(invert_transformations(rf_forecast), index=test_index)
-plt.figure(figsize=(15, 5))
+plt.subplot(6, 1, 4)
 plt.plot(df.Active, label='data')
 plt.plot(rf_forecast_series, label='rf')
 plt.title('Random Forest - RMSE: %.2f' % rmse(test_set.flatten(), rf_forecast))
 plt.legend()
-plt.show()
 
 # XGBoost
-xgb_model = XGBRegressor(n_estimators=500)
+xgb_model = XGBRegressor(n_estimators=100)
 xgb_model.fit(train_set, train_index)
 xgb_forecast = xgb_model.predict(test_set)
 xgb_forecast_series = pd.Series(invert_transformations(xgb_forecast), index=test_index)
-plt.figure(figsize=(15, 5))
+plt.subplot(6, 1, 5)
 plt.plot(df.Active, label='data')
 plt.plot(xgb_forecast_series, label='xgb')
 plt.title('XGBoost - RMSE: %.2f' % rmse(test_set.flatten(), xgb_forecast))
 plt.legend()
+
+# compare models
+plt.subplot(6, 1, 6)
+plt.plot(df.Active[train_size:], label='data')
+plt.plot(sarima_forecast_series, label='sarima')
+plt.plot(lstm_forecast_series, label='lstm')
+plt.plot(mlp_forecast_series, label='mlp')
+# take out random forest and xgboost from the plot, because they are really worse than the rest
+# plt.plot(rf_forecast_series, label='forest')
+# plt.plot(xgb_forecast_series, label='xgb')
+plt.title('Comparison of models')
+plt.legend()
 plt.show()
 
+test_set = test_set.flatten()
+print('------------------\nMODEL COMPARISON\n------------------')
+print('SARIMA - RMSE: %.4f' % rmse(test_set, sarima_forecast))
+print('LSTM - RMSE: %.4f' % rmse(test_set, lstm_forecast.flatten()))
+print('MLP - RMSE: %.4f' % rmse(test_set, mlp_forecast.flatten()))
+print('Random Forest - RMSE: %.4f' % rmse(test_set, rf_forecast))
+print('XGBoost - RMSE: %.4f' % rmse(test_set, xgb_forecast))
+print(
+    'MLPvsLSTM - DM: p-value: %.4f DM: %.4f ' % dm_test(test_set, lstm_forecast.flatten(), mlp_forecast.flatten(), h=1,
+                                                        crit='MSE'))
+# MLPvsLSTM - DM: p-value: 5.1069 DM: 0.0006
+# with 5% significance level, the p-value is less than 0.025 and z-value is greater than 1.96,
+# so we can reject the null hypothesis that the two models have the same performance
+print(
+    'MLP seems better than LSTM, but in the DM test the p-value is not significant,'
+    ' so we reject the null hypothesis that the two models have the same performance.')
